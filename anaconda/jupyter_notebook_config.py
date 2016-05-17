c3a917fe9b693b471622554b6c271d105e7c420c
Siyuan LIU <weifengzi2009@gmail.com>
2016-05-17 20:27:02 +0800

    comment out the password config

--- anaconda/jupyter_notebook_config.py
+++ anaconda/jupyter_notebook_config.py
@@ -198,7 +198,7 @@ c.NotebookApp.open_browser = False
 #   from notebook.auth import passwd; passwd()
 #
 # The string should be of the form type:salt:hashed-password.
-c.NotebookApp.password = u'sha1:<your salt>:<your hashed password>'
+# c.NotebookApp.password = u'sha1:<your salt>:<your hashed password>'
 
 # The port the notebook server will listen on.
 c.NotebookApp.port = 8888
